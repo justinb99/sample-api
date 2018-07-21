fbd14e72572ad8321e0542647b98b0b1a83e2813
Justin B <justinb99@users.noreply.github.com>
2018-07-21 15:10:55 -0500

    revised IT for Jersey server

--- integration-tests/src/test/java/justinb99/sampleapi/integrationtests/MyResourceIT.java
+++ integration-tests/src/test/java/justinb99/sampleapi/integrationtests/MyResourceIT.java
@@ -1,62 +1,38 @@
 package justinb99.sampleapi.integrationtests;
 
+import io.logz.guice.jersey.JerseyServer;
 import justinb99.sampleapi.service.Main;
-import org.glassfish.grizzly.http.server.HttpServer;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
 
 import static com.jayway.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
 
 public class MyResourceIT {
 
-  private HttpServer server;
-  private WebTarget target;
+  private static JerseyServer server;
 
-  @Before
-  public void setUp() throws Exception {
-    // start the server
+  @BeforeClass
+  public static void beforeClass() throws Exception {
     server = Main.startServer();
-    // create the client
-    Client c = ClientBuilder.newClient();
-
-    // uncomment the following line if you want to enable
-    // support for JSON in the client (you also have to uncomment
-    // dependency on jersey-media-json module in pom.xml and Main.startServer())
-    // --
-    // c.configuration().enable(new org.glassfish.jersey.media.json.JsonJaxbFeature());
-
-    target = c.target(Main.BASE_URI);
   }
 
-  @After
-  public void tearDown() throws Exception {
-    server.shutdownNow();
-  }
-
-  /**
-   * Test to see that the message "Got it!" is sent in the response.
-   */
-  @Test
-  public void testGetIt() {
-    String responseMsg = target.path("myresource").request().get(String.class);
-    assertEquals("Got it!", responseMsg);
+  @AfterClass
+  public static void afterClass() throws Exception {
+    server.stop();
   }
 
   @Test
   public void getMyResource() {
     String response = given()
-      .get(Main.BASE_URI + "myresource")
+      .get("http://localhost:8080/myresource")
       .then()
       .log().ifValidationFails()
       .statusCode(200)
       .extract()
       .body().asString();
-    assertEquals("Got it!", response);
+    assertEquals("Hello World!", response);
   }
+
 }
--- service/src/main/java/justinb99/sampleapi/service/Main.java
+++ service/src/main/java/justinb99/sampleapi/service/Main.java
@@ -1,72 +1,47 @@
 package justinb99.sampleapi.service;
 
-import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Module;
-import com.google.inject.Provides;
 import io.logz.guice.jersey.JerseyModule;
 import io.logz.guice.jersey.JerseyServer;
 import io.logz.guice.jersey.configuration.JerseyConfiguration;
-import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
-import org.glassfish.jersey.server.ResourceConfig;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.inject.Named;
-import java.io.IOException;
-import java.net.URI;
 import java.util.ArrayList;
 
 /**
  * Main class.
  */
 public class Main {
-  // Base URI the Grizzly HTTP server will listen on
-  public static final String BASE_URI = "http://localhost:8080/myapp/";
 
-  /**
-   * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
-   *
-   * @return Grizzly HTTP server.
-   */
-  public static HttpServer startServer() {
-    // create a resource config that scans for JAX-RS resources and providers
-    // in justinb99.sampleapi package
-    final ResourceConfig rc = new ResourceConfig().packages("justinb99.sampleapi");
+  private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
-    // create and start a new instance of grizzly http server
-    // exposing the Jersey application at BASE_URI
-    return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
-  }
-
-  /**
-   * Main method.
-   *
-   * @param args
-   * @throws IOException
-   */
-  public static void main(String[] args) throws Exception {
-    var logger = LoggerFactory.getLogger(Main.class);
-
+  public static JerseyServer startServer() throws Exception {
     var configuration = JerseyConfiguration.builder()
       .addPackage("justinb99.sampleapi.service.resource")
       .addPort(8080)
       .build();
-
-    logger.info("\n\n\n*******contextPath='" + configuration.getContextPath() + "'\n\n\n");
 
     var modules = new ArrayList<Module>();
     modules.add(new JerseyModule(configuration));
     modules.add(new ServiceModule());
     //TODO: add more modules
 
-    JerseyServer server = Guice.createInjector(modules)
+    var server = Guice.createInjector(modules)
       .getInstance(JerseyServer.class);
 
     server.start();
+
+    return server;
+  }
+
+  public static void main(String[] args) throws Exception {
+    var server = startServer();
     logger.info("Jersey app started.  Hit enter to stop it...");
     System.in.read();
     server.stop();
   }
+
 }
 
