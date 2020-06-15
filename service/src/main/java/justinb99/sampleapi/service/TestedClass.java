046551013ec5afdde5c6d27c526f5c99bc85ad66
Justin B <justinb99@users.noreply.github.com>
2020-06-15 10:53:13 -0500

    Add tested and untested class

---
+++ service/src/main/java/justinb99/sampleapi/service/TestedClass.java
@@ -0,0 +1,11 @@
+package justinb99.sampleapi.service;
+
+public class TestedClass {
+
+  public static String getEvenOdd(int value) {
+    if (value % 2 == 0)
+      return "even";
+    else
+      return "odd";
+  }
+}
---
+++ service/src/main/java/justinb99/sampleapi/service/UnTestedClass.java
@@ -0,0 +1,11 @@
+package justinb99.sampleapi.service;
+
+public class UnTestedClass {
+
+  public static String getEvenOdd(int value) {
+    if (value % 2 == 0)
+      return "even";
+    else
+      return "odd";
+  }
+}
