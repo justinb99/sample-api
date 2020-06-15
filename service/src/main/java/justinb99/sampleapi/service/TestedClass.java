0855195b36bfb60200a2071079e7ee1ab98795c3
Justin B <justinb99@users.noreply.github.com>
2020-06-15 10:58:26 -0500

    add private constructor

--- service/src/main/java/justinb99/sampleapi/service/TestedClass.java
+++ service/src/main/java/justinb99/sampleapi/service/TestedClass.java
@@ -2,6 +2,8 @@ package justinb99.sampleapi.service;
 
 public class TestedClass {
 
+  private TestedClass() {}
+
   public static String getEvenOdd(int value) {
     if (value % 2 == 0)
       return "even";
